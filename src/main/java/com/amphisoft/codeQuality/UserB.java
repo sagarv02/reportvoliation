163c2f1e2960dc5e2651f83b30218c4b0e6ad5fa
amphisoft <vanmathi@amphisoft.co.in>
2019-02-27 10:34:06 +0530

    cpd

---
+++ src/main/java/com/amphisoft/codeQuality/UserB.java
@@ -0,0 +1,20 @@
+package com.amphisoft.codeQuality;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.Scanner;
+import java.util.Scanner;
+
+public class UserB {
+	public boolean validateEmail(String email) {
+		User user = new User();
+		String dummyObj;
+		UserBO userBO = new UserBO();
+        Scanner scan = new Scanner(System.in);
+		Pattern pattern=Pattern.compile("^([A-Za-z]{1})([_.A-Za-z0-9])*@([A-Za-z])+(\\.)([A-Za-z]{2,6})$");
+		Matcher matcher=pattern.matcher(email);
+		if(matcher.matches())
+			return true;
+		return false;
+	}
+}
