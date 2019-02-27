89bf8b46a7a021964e4e462b02aa2d4792609c65
amphisoft <vanmathi@amphisoft.co.in>
2019-02-27 09:50:37 +0530

    cpd

--- src/main/java/com/amphisoft/codeQuality/UserBO.java
+++ src/main/java/com/amphisoft/codeQuality/UserBO.java
@@ -5,6 +5,10 @@ import java.util.regex.Pattern;
 
 public class UserBO {
 	public boolean validateEmail(String email) {
+		User user = new User();
+		String dummyObj;
+		UserBO userBO = new UserBO();
+        Scanner scan = new Scanner(System.in);
 		Pattern pattern=Pattern.compile("^([A-Za-z]{1})([_.A-Za-z0-9])*@([A-Za-z])+(\\.)([A-Za-z]{2,6})$");
 		Matcher matcher=pattern.matcher(email);
 		if(matcher.matches())
